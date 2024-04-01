9e447ef9e741933b337450edec05fc75c12a04c7
Luciana11lm <lucianam754.lm@gmail.com>
2024-04-01 15:12:35 +0300

    Updated new folders

--- lib/app_view.dart
+++ lib/app_view.dart
@@ -4,6 +4,8 @@ import 'package:menu_app/blocs/authentication_bloc/authentication_bloc.dart';
 import 'package:menu_app/screens/auth/views/welcome_screen.dart';
 import 'package:menu_app/screens/home/views/home_screen.dart';
 
+import 'screens/auth/blocs/sign_in_bloc/sign_in_bloc.dart';
+
 class MyAppView extends StatelessWidget {
   const MyAppView({super.key});
 
@@ -13,15 +15,19 @@ class MyAppView extends StatelessWidget {
         title: 'Menu',
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
-            colorScheme: ColorScheme.light(
-                background: Colors.grey.shade100,
+            colorScheme: const ColorScheme.light(
+                background: Color(0xFFE9EDE4),
                 onBackground: Colors.black,
                 primary: Colors.blue,
                 onPrimary: Colors.white)),
         home: BlocBuilder<AuthenticationBloc, AuthenticationState>(
           builder: (context, state) {
             if (state.status == AuthenticationStatus.authenticated) {
-              return const HomeSceen();
+              return BlocProvider(
+                create: (context) => SignInBloc(
+                    context.read<AuthenticationBloc>().userRepository),
+                child: const HomeSceen(),
+              );
             } else {
               return const WelcomeScreen();
             }
--- lib/screens/home/views/home_screen.dart
+++ lib/screens/home/views/home_screen.dart
@@ -1,10 +1,72 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:menu_app/screens/auth/blocs/sign_in_bloc/sign_in_bloc.dart';
 
 class HomeSceen extends StatelessWidget {
   const HomeSceen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.background,
+        title: Row(
+          children: [
+            Image.asset('assets/MENU.png', scale: 10),
+            const SizedBox(
+              width: 8,
+            ),
+            const Text(
+              'MENU',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+            )
+          ],
+        ),
+        actions: [
+          IconButton(onPressed: () {}, icon: const Icon(CupertinoIcons.cart)),
+          IconButton(
+              onPressed: () {
+                context.read<SignInBloc>().add(SignOutRequired());
+              },
+              icon: const Icon(CupertinoIcons.arrow_right_to_line)),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: GridView.builder(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                crossAxisSpacing: 16,
+                mainAxisSpacing: 16,
+                childAspectRatio: 9 / 16),
+            itemCount: 8,
+            itemBuilder: (context, int i) {
+              return Container(
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(20),
+                      boxShadow: [
+                        BoxShadow(
+                            color: Colors.grey.shade300,
+                            offset: const Offset(3, 3))
+                      ]),
+                  child: Column(
+                    children: [
+                      Image.asset('assets/b1.png'),
+                      Row(
+                        children: [
+                          Container(
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(30)),
+                              child: const Text('Burger cu pui'))
+                        ],
+                      )
+                    ],
+                  ));
+            }),
+      ),
+    );
   }
 }
